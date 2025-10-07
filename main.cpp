874444f8dca3201f97b01ab30c6985876019c8ce
Norich-1988 <egorpogrebnyak@gmail.com>
2025-10-08 00:20:40 +0300

    Улучшено коснольное меню, функция по удалению занесена в switch

--- main.cpp
+++ main.cpp
@@ -65,6 +65,7 @@ int main() {
         std::cout << "Меню:\n";
         std::cout << "1. Добавить студента\n";
         std::cout << "2. Вывести список студентов\n";
+        std::cout << "3. Удалить студента\n";
         std::cout << "0. Выход\n";
         std::cout << "Выберите действие: ";
         std::cin >> choice;
@@ -75,6 +76,9 @@ int main() {
                 break;
             case 2:
                 displayStudents(database);
+                break;
+            case 3:
+                removeStudent(database);
                 break;
             case 0:
                 std::cout << "Выход из программы.\n";
